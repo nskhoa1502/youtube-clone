e6f10d2b11be3f7e3647824e93c6dcb0fd726253
nskhoa1502 <nskhoa1502@gmail.com>
2023-05-31 21:47:52 +0700

    #9 finish login page

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,8 +1,10 @@
 import React from "react";
+
 import styled from "styled-components";
 
 const Container = styled.div`
   display: flex;
+  flex-direction: column;
   align-items: center;
   justify-content: center;
   height: calc(100vh - 60px); //60px is the height of navbar
@@ -18,14 +20,71 @@ const Wrapper = styled.div`
   gap: 10px;
 `;
 
-const Title = styled.h1``;
+const Title = styled.h1`
+  font-size: 24px;
+`;
 
-const SubTitle = styled.h2;
+const SubTitle = styled.h2`
+  font-size: 20px;
+  font-weight: 300;
+`;
+
+const Input = styled.input`
+  border: 1px solid ${({ theme }) => theme.soft};
+  border-radius: 3px;
+  padding: 10px;
+  background-color: transparent;
+  width: 100%;
+`;
+
+const Button = styled.button`
+  border-radius: 3px;
+  border: none;
+  padding: 10px 20px;
+  font-weight: 500;
+  cursor: pointer;
+  background-color: ${({ theme }) => theme.soft};
+  color: ${({ theme }) => theme.textSoft};
+`;
+
+const More = styled.div`
+  display: flex;
+  margin-top: 10px;
+  font-size: 12px;
+  color: ${({ theme }) => theme.textSoft};
+`;
+
+const Links = styled.div`
+  margin-left: 50px;
+`;
+
+const Link = styled.span`
+  margin-left: 30px;
+`;
 
 const Login = () => {
   return (
     <Container>
-      <Wrapper>Logindasdfaslkdfjsldkf</Wrapper>
+      <Wrapper>
+        <Title>Sign in</Title>
+        <SubTitle>to continue to CloneTube</SubTitle>
+        <Input placeholder="username"></Input>
+        <Input placeholder="password" type="password"></Input>
+        <Button>Sign in</Button>
+        <Title>or</Title>
+        <Input placeholder="username"></Input>
+        <Input placeholder="email"></Input>
+        <Input placeholder="password" type="password"></Input>
+        <Button>Sign up</Button>
+      </Wrapper>
+      <More>
+        English(USA)
+        <Links>
+          <Link>Help</Link>
+          <Link>Privacy</Link>
+          <Link>Terms</Link>
+        </Links>
+      </More>
     </Container>
   );
 };
