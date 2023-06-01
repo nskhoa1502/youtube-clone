06d3b8b0a12b96b6f053f2cebf17ed10ff11aefc
nskhoa1502 <nskhoa1502@gmail.com>
2023-06-02 03:51:26 +0700

    #13 add user routes

---
+++ backend/controllers/user.js
@@ -0,0 +1,13 @@
+exports.putUpdateUser = async (req, res, next) => {};
+
+exports.deleteUser = async (req, res, next) => {};
+
+exports.getUser = async (req, res, next) => {};
+
+exports.putSubscribe = async (req, res, next) => {};
+
+exports.putUnsubscribe = async (req, res, next) => {};
+
+exports.postLike = async (req, res, next) => {};
+
+exports.postDislike = async (req, res, next) => {};
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,4 +1,34 @@
 const express = require("express");
-const router = express();
+const router = express.Router();
+const {
+  putUpdateUser,
+  deleteUser,
+  getUser,
+  putSubscribe,
+  putUnsubscribe,
+  postLike,
+  postDislike,
+} = require("../controllers/user");
+
+// Update user
+router.put("/:id", putUpdateUser);
+
+// Delete user
+router.delete("/find/:id", deleteUser);
+
+// Get a user
+router.get("/find/:id", getUser);
+
+// Subscribe a user
+router.post("/sub/:id", putSubscribe);
+
+// Unsubscribe a user
+router.post("/unsub/:id", putUnsubscribe);
+
+// Like a video
+router.post("/like/:videoId", postLike);
+
+// Dislike a video
+router.post("/dislike/:videoId", postDislike);
 
 module.exports = router;
