aacdc58e4578639b6168f478d8c1f81107e83669
nskhoa1502 <nskhoa1502@gmail.com>
2023-06-02 02:59:21 +0700

    #11 add user, comment, video models and some basic routes

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const authRoutes = require("./routes/auth");
+const userRoutes = require("./routes/user");
+const videoRoutes = require("./routes/video");
+const commentRoutes = require("./routes/comment");
 
 const app = express();
 dotenv.config();
@@ -16,6 +20,12 @@ const connect = () => {
       throw err;
     });
 };
+
+// routes
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/videos", videoRoutes);
+app.use("/api/comments", commentRoutes);
 
 mongoose.connection.on("disconnected", () => {
   console.log("mongoDB disconnected");
---
+++ backend/models/User.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+    },
+    img: {
+      type: String,
+    },
+    subscribers: {
+      type: Number,
+      default: 0,
+    },
+    subscribedUsers: {
+      type: [String],
+    },
+    fromGoogle: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("User", UserSchema);
